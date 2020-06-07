afd05fcdedc4daebf5c6371d0fc645b0af3489ca
Andy Kuck <andy@DESKTOP-DKLKKGG.localdomain>
2020-06-07 14:29:33 -0500

    Small changes about HTTPS redirect

--- list_contacts/urls.py
+++ list_contacts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('/robots.txt/', views.robots, name='robots'),
-	path('/sitemap.xml/', views.sitemap, name='sitemap')
+	path('robots.txt', views.robots, name='robots'),
+	path('sitemap.xml', views.sitemap, name='sitemap')
 ]
 
 
