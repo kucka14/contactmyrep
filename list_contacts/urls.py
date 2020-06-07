3965bdfefd31b1671e7c27d50ca4dfc3e6a681df
Andy Kuck <andy@DESKTOP-DKLKKGG.localdomain>
2020-06-07 13:53:45 -0500

    Fixed single error with new code

--- list_contacts/urls.py
+++ list_contacts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('/robots.txt/', views.robots, name='robots')
+	path('/robots.txt/', views.robots, name='robots'),
 	path('/sitemap.xml/', views.sitemap, name='sitemap')
 ]
 
