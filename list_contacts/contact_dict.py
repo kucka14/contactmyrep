74eecbf300030318d2ac7fccc239350087958d0a
Andy Kuck <andy@DESKTOP-DKLKKGG.localdomain>
2020-06-09 14:06:48 -0500

    Added all senator contact info to the contact_dict

--- list_contacts/contact_dict.py
+++ list_contacts/contact_dict.py
@@ -1,3 +1,161 @@
 contact_dict = {
-	"Richard J. Durbin":"https://www.durbin.senate.gov/coronavirus-contact",
+
+# Senators	
+	# Alaska (AK)
+		'Lisa Murkowski':'https://www.murkowski.senate.gov/contact/email',
+		'Dan Sullivan':'https://www.sullivan.senate.gov/contact/email',
+	# Alabama (AL)
+		'Doug Jones':'https://www.jones.senate.gov/contact/email-doug',
+		'Richard C. Shelby':'https://www.shelby.senate.gov/public/index.cfm/emailsenatorshelby',
+	# Arkansas (AR)
+		'John Boozman':'https://www.boozman.senate.gov/public/index.cfm/contact',
+		'Tom Cotton':'https://www.cotton.senate.gov/?p=contact',
+	# American Samoa (AS)
+	# Arizona (AZ)
+		'Kyrsten Sinema':'https://www.sinema.senate.gov/contact-kyrsten',
+		'Martha McSally':'https://www.mcsally.senate.gov/contact/email-martha',
+	# California (CA)
+		'Dianne Feinstein':'https://www.feinstein.senate.gov/public/index.cfm/e-mail-me',
+		'Kamala D. Harris':'https://www.harris.senate.gov/contact/email',
+	# Colorado (CO)
+		'Cory Gardner':'https://www.gardner.senate.gov/contact-cory/email-cory',
+		'Michael F. Bennet':'https://www.bennet.senate.gov/public/index.cfm/index.cfm/write-to-michael',
+	# Connecticut (CT)
+		'Richard Blumenthal':'https://www.blumenthal.senate.gov/contact',
+		'Christopher Murphy':'https://www.murphy.senate.gov/contact',
+	# District of Columbia (DC)
+	# Delaware (DE)
+		'Thomas R. Carper':'https://www.carper.senate.gov/public/index.cfm/email-senator-carper',
+		'Christopher A. Coons':'https://www.coons.senate.gov/contact/share-your-opinion',
+	# Florida (FL)
+		'Rick Scott':'https://www.rickscott.senate.gov/contact_rick',
+		'Marco Rubio':'https://www.rubio.senate.gov/public/index.cfm/contact',
+	# Georgia (GA)
+		'David Perdue':'https://www.perdue.senate.gov/connect/email',
+		'Kelly Loeffler':'https://www.loeffler.senate.gov/connect/email-kelly',
+	# Guam (GU)
+	# Hawaii (HI)
+		'Brian Schatz':'https://www.schatz.senate.gov/contact',
+		'Mazie K. Hirono':'https://www.hirono.senate.gov/contact',
+	# Iowa (IA)
+		'Chuck Grassley':'https://www.grassley.senate.gov/constituents/questions-and-comments',
+		'Joni Ernst':'https://www.ernst.senate.gov/public/index.cfm/contact',
+	# Idaho (ID)
+		'James E. Risch':'https://www.risch.senate.gov/public/index.cfm/contact?p=Email',
+		'Mike Crapo':'',
+	# Illinois (IL)
+		'Richard J. Durbin':'https://www.crapo.senate.gov/contact/email-me',
+		'Tammy Duckworth':'https://www.duckworth.senate.gov/connect/email-tammy',
+	# Indiana (IN)
+		'Mike Braun':'https://www.braun.senate.gov/contact-mike',
+		'Todd Young':'https://www.young.senate.gov/contact/email-todd',
+	# Kansas (KS)
+		'Pat Roberts':'https://www.roberts.senate.gov/public/index.cfm/emailpat',
+		'Jerry Moran':'https://www.moran.senate.gov/public/index.cfm/e-mail-jerry',
+	# Kentucky (KY)
+		'Mitch McConnell':'https://www.mcconnell.senate.gov/public/index.cfm/contactform',
+		'Rand Paul':'https://www.paul.senate.gov/connect/email-rand',
+	# Louisiana (LA)
+		'John Kennedy':'https://www.kennedy.senate.gov/public/email-me',
+		'Bill Cassidy':'https://www.cassidy.senate.gov/contact',
+	# Massachusetts (MA)
+		'Elizabeth Warren':'https://www.warren.senate.gov/contact/shareyouropinion',
+		'Edward J. Markey':'https://www.markey.senate.gov/contact',
+	# Maryland (MD)
+		'Chris Van Hollen':'https://www.vanhollen.senate.gov/contact/email',
+		'Benjamin L. Cardin':'https://www.cardin.senate.gov/contact/email-ben',
+	# Maine (ME)
+		'Angus S. King Jr.':'https://www.king.senate.gov/contact',
+		'Susan M. Collins':'https://www.collins.senate.gov/contact',
+	# Michigan (MI)
+		'Gary C. Peters':'https://www.peters.senate.gov/contact/email-gary',
+		'Debbie Stabenow':'https://www.stabenow.senate.gov/contact',
+	# Minnesota (MN)
+		'Amy Klobuchar':'https://www.klobuchar.senate.gov/public/index.cfm/email-amy',
+		'Tina Smith':'https://www.smith.senate.gov/share-your-opinion',
+	# Missouri (MO)
+		'Roy Blunt':'https://www.blunt.senate.gov/contact/contact-roy',
+		'Josh Hawley':'https://www.hawley.senate.gov/contact-senator-hawley',
+	# Northern Mariana Islands (MP)
+	# Mississippi (MS)
+		'Roger F. Wicker':'https://www.wicker.senate.gov/public/index.cfm/contact',
+		'Cindy Hyde-Smith':'https://www.hydesmith.senate.gov/contact-senator',
+	# Montana (MT)
+		'Jon Tester':'https://www.tester.senate.gov/contact/',
+		'Steve Daines':'https://www.daines.senate.gov/connect/email-steve',
+	# North Carolina (NC)
+		'Thom Tillis':'https://www.tillis.senate.gov/email-me',
+		'Richard Burr':'https://www.burr.senate.gov/contact/email',
+	# North Dakota (ND)
+		'John Hoeven':'https://www.hoeven.senate.gov/contact/email-the-senator',
+		'Kevin Cramer':'https://www.cramer.senate.gov/contact_kevin',
+	# Nebraska (NE)
+		'Deb Fischer':'https://www.fischer.senate.gov/public/?p=email-deb',
+		'Ben Sasse':'https://www.sasse.senate.gov/public/index.cfm/email-ben',
+	# New Hampshire (NH)
+		'Margaret Wood Hassan':'https://www.hassan.senate.gov/contact/email',
+		'Jeanne Shaheen':'https://www.shaheen.senate.gov/contact/contact-jeanne',
+	# New Jersey (NJ)
+		'Robert Menendez':'https://www.menendez.senate.gov/contact',
+		'Cory A. Booker':'https://www.booker.senate.gov/contact/write-to-cory/form',
+	# New Mexico (NM)
+		'Martin Heinrich':'https://www.heinrich.senate.gov/contact/write-martin',
+		'Tom Udall':'https://www.tomudall.senate.gov/contact/email-tom',
+	# Nevada (NV)
+		'Jacky Rosen':'https://www.rosen.senate.gov/contact_jacky',
+		'Catherine Cortez Masto':'https://www.cortezmasto.senate.gov/contact/connect',
+	# New York (NY)
+		'Kirsten E. Gillibrand':'https://www.gillibrand.senate.gov/contact/email-me',
+		'Charles E. Schumer':'https://www.schumer.senate.gov/contact/email-chuck',
+	# Ohio (OH)
+		'Rob Portman':'https://www.portman.senate.gov/meet/contact',
+		'Sherrod Brown':'https://www.brown.senate.gov/contact/email',
+	# Oklahoma (OK)
+		'James Lankford':'https://www.lankford.senate.gov/contact/email',
+		'James M. Inhofe':'https://www.inhofe.senate.gov/contact/email-jim',
+	# Oregon (OR)
+		'Ron Wyden':'https://www.wyden.senate.gov/contact/email-ron',
+		'Jeff Merkley':'https://www.merkley.senate.gov/contact',
+	# Pennsylvania (PA)
+		'Patrick J. Toomey':'https://www.toomey.senate.gov/?p=contact',
+		'Robert P. Casey Jr.':'https://www.casey.senate.gov/contact/',
+	# Puerto Rico (PR)
+	# Rhode Island (RI)
+		'Sheldon Whitehouse':'https://www.whitehouse.senate.gov/contact/email-sheldon',
+		'Jack Reed':'https://www.reed.senate.gov/contact/email',
+	# South Carolina (SC)
+		'Lindsey Graham':'https://www.lgraham.senate.gov/public/index.cfm?p=contact-form',
+		'Tim Scott':'https://www.scott.senate.gov/contact/email-me',
+	# South Dakota (SD)
+		'Mike Rounds':'https://www.rounds.senate.gov/contact/email-mike',
+		'John Thune':'https://www.thune.senate.gov/public/index.cfm/contact',
+	# Tennessee (TN)
+		'Lamar Alexander':'https://www.alexander.senate.gov/public/index.cfm/email',
+		'Marsha Blackburn':'https://www.blackburn.senate.gov/contact_marsha',
+	# Texas (TX)
+		'John Cornyn':'https://www.cornyn.senate.gov/contact',
+		'Ted Cruz':'https://www.cruz.senate.gov/?p=form&id=16',
+	# Utah (UT)
+		'Mike Lee':'https://www.lee.senate.gov/public/index.cfm/contact',
+		'Mitt Romney':'https://www.romney.senate.gov/contact',
+	# Virginia (VA)
+		'Mark R. Warner':'https://www.warner.senate.gov/public/index.cfm?p=ContactPage',
+		'Tim Kaine':'https://www.kaine.senate.gov/contact/share-your-opinion',
+	# Virgin Islands (VI)
+	# Vermont (VT)
+		'Patrick J. Leahy':'https://www.leahy.senate.gov/contact/',
+		'Bernie Sanders':'https://www.sanders.senate.gov/contact/comment',
+	# Washington (WA)
+		'Patty Murray':'https://www.murray.senate.gov/public/index.cfm/mobile/contactme',
+		'Maria Cantwell':'https://www.cantwell.senate.gov/contact/email/form',
+	# Wisconsin (WI)
+		'Ron Johnson':'https://www.ronjohnson.senate.gov/public/index.cfm/email-the-senator',
+		'Tammy Baldwin':'https://www.baldwin.senate.gov/feedback',
+	# West Virginia (WV)
+		'Shelley Moore Capito':'https://www.capito.senate.gov/contact/share-your-opinion',
+		'Joe Manchin III':'https://www.manchin.senate.gov/contact-joe/email-joe',
+	# Wyoming (WY)
+		'John Barrasso':'https://www.barrasso.senate.gov/public/index.cfm/contact-form',
+		'Michael B. Enzi':'https://www.enzi.senate.gov/public/index.cfm/e-mail-senator-enzi',
+	
 }
