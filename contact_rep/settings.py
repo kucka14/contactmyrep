ad123b72d02f8e5912d2f0f0f71f1c1b5c63c43a
Andy Kuck <andy@DESKTOP-DKLKKGG.localdomain>
2020-06-30 06:16:28 -0500

    Removed possibly superfluous line from settings about https

--- contact_rep/settings.py
+++ contact_rep/settings.py
@@ -130,8 +130,9 @@ STATICFILES_DIR = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 
 
